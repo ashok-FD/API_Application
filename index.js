07316071fdc4b54f292337a5b4e115575645eb61
Ashok <53075364+ashok-FD@users.noreply.github.com>
2019-12-06 12:36:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ app.use(morgan('dev'));
 //Router
 const PersonRouter = require('./PersonsRoute');
 app.use('/persons',PersonRouter);
-app.listen(2000, () =>{console.log("server is running at 5000")});
+app.listen(2000, () =>{console.log("server is running at 2000")});
 
 //db connection server
 mongoose.set('useNewUrlParser',true);
 mongoose.set('useUnifiedTopology',true);
+mongoose.set("useCreateIndex", true);
 mongoose.connect(process.env.DbName, err => {
   if (err) {
     console.log("db UnConnect");
